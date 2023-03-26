296c1b133cd22a4e093fafc63fe59e43cf146eb8
Jim <jimw54321@gmail.com>
2023-03-26 12:12:43 -0400

    day19_theme_clock

--- html_css_js/day19_theme_clock/script.js
+++ html_css_js/day19_theme_clock/script.js
@@ -21,19 +21,22 @@ toggle.addEventListener('click', (e) => {
 
 function setTime() {
     const time = new Date()
-    console.log(time)
     const month = time.getMonth()
     const day = time.getDay();
     const date = time.getDate()
     const hours = time.getHours()
-    const hoursForClock = hours % 12
+    var hoursForClock = hours % 12
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
     const ampm = hours >= 12 ? 'PM' : 'AM'
     
+    if(hoursForClock === 0 ) {
+        hoursForClock = 12
+    }
+
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
 
     timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
